83c241a5a5f75d16216309271da8a2b7e34102ec
Rishabh Desai <desai.rishab@gmail.com>
2021-02-21 19:25:16 +0530

    .

--- Day_09/src/genericCode/Program.java
+++ Day_09/src/genericCode/Program.java
@@ -1,6 +1,42 @@
+//if we want to write generic type-safe code, then we should use generics in java
+//It gives strong type checking at compile time
+//it completely eliminates type-casting
+//we can write generic data-structure and algorithms
+
+
 package genericCode;
 
+class Square<T> {  // T :  Type parameter
+	private T sides;
+
+	public T getSides() {
+		return sides;
+	}
+
+	public void setSides(T sides) {
+		this.sides = sides;
+	}
+
+}
+
 public class Program {
-	
+	public static void main(String[] args) {
+
+		// Integer
+		Square<Integer> s1 = new Square<>();  //<Integer> : Type argument
+		s1.setSides(5);
+		System.out.println(s1.getSides());
+
+		// Float
+		Square<Float> f1 = new Square<>();
+		f1.setSides(1.5f);
+		System.out.println(f1.getSides());
+
+		// Double
+		Square<Double> d1 = new Square<>();
+		d1.setSides(1.5);
+		System.out.println(d1.getSides());
+
+	}
 
 }
